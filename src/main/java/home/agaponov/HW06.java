d89a211c346d7e68acd49405d4e54b4e7c189901
Agaponov Maxym <maxym.agaponov@gmail.com>
2018-02-04 13:07:51 +0200

    Firs commit

--- src/main/java/home/agaponov/HW06.java
+++ src/main/java/home/agaponov/HW06.java
@@ -1,6 +1,22 @@
 package home.agaponov;
 
 
-public class HW06 {
+import java.util.Scanner;
 
+public class HW06 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter x");
+        double x = scan.nextDouble();
+
+        System.out.println("Enter y");
+        double y = scan.nextDouble();
+
+        System.out.println("Enter z");
+        double z = scan.nextDouble();
+
+        double result = (x*x) + (y*y) + (z*z);
+
+        System.out.println("Длина вектора " +"("+x+y+z+")"+" = "+Math.sqrt(result));
+    }
 }
