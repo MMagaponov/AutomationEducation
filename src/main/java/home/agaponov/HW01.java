79099c7a09c1fe208366804cabc30595ac78d643
MMagaponov <git>
2018-02-05 16:01:57 +0200

    Minor changes

--- src/main/java/home/agaponov/HW01.java
+++ src/main/java/home/agaponov/HW01.java
@@ -3,10 +3,10 @@ package home.agaponov;
 
 public class HW01 {
     public static void main(String[] args) {
-        int x = 10;
-        int y = 5;
-        int z = 3;
-        int n = 9;
+        long x = 10;
+        long y = 5;
+        long z = 3;
+        long n = 9;
         long t;
          t = x + (y - 100) * z / n;
         System.out.println("Result = " + t);
--- src/main/java/home/agaponov/HW03.java
+++ src/main/java/home/agaponov/HW03.java
@@ -3,10 +3,10 @@ package home.agaponov;
 
 public class HW03 {
     public static void main(String[] args) {
-        int x = 10;
-        int y = 5;
-        int z = 3;
-        int n = 9;
+        float x = 10;
+        float y = 5;
+        float z = 3;
+        float n = 9;
         float t;
         t = x * (y / (z-1) + 555) - n;
         System.out.println("Result = " + t);
